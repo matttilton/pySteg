fc656f9fc96be3521a8abfaf1f71f4e324860170
Matthew Tilton <mtilton1996@gmail.com>
2016-07-25 00:11:38 -0500

    Added docstings. Other minor improvements. Nightly commit.

--- pySteg.py
+++ pySteg.py
@@ -1,27 +1,34 @@
-# TODO Consolidate file. DONE
+"""This utility aims to provide an easy way to encode text inside of images."""
+# TODO Write test cases.
 # TODO Make classes.
-# TODO Add variable for file name.
 # TODO Add variable for file type.
 # TODO Add variable for path to master.
 # TODO Add variable for path to result.
 # TODO Add interface.
-# TODO Add offset. In Progress
-# TODO Add random seed
-# TODO Add timer. DONE
-# TODO Add error check. DONE
-# TODO Organize
-# TODO Add Docstrings
-# TODO Add check for max data size. DONE
+# TODO Add offset.
+# TODO Add random seed.
+# TODO Add Docstrings. DONE.
+# TODO Add check for max data size. DONE.
+# TODO Add timer. DONE.
+# TODO Add error check. DONE.
+# TODO Add variable for file name. DONE.
+# TODO Consolidate file. DONE.
+# TODO Organize. DONE.
 
 from cImage import FileImage
 from cImage import EmptyImage
-from cImage import Pixel
 from time import time
 
+EncodedFileName = "Encoded"
 key = FileImage("master.png")
-backup = FileImage("Encoded.png")
+try:
+    backup = FileImage(EncodedFileName + ".png")
+except:
+    backup = None
+
 
 def Encode(data):
+    """Take binary data and add it to an image."""
     result = EmptyImage(key.getWidth(), key.getHeight())
     count = 0
     for row in range(key.getHeight()):
@@ -36,10 +43,11 @@ def Encode(data):
                 result.setPixel(col, row, newPixel)
             else:
                 result.setPixel(col, row, keyPixel)
-    result.save("Encoded.png")
+    result.save(EncodedFileName + ".png")
 
 
 def Decode():
+    """Extract binary data from image."""
     encoded = FileImage("Encoded.png")
     result = []
     for row in range(encoded.getHeight()):
@@ -58,11 +66,13 @@ def Decode():
 
 
 def textToBinary(data):
+    """Convert text to binary."""
     result = ''.join(format(ord(x), '08b') for x in data)
     return str(result)
 
 
 def binaryToText(data):
+    """Convert binary to text."""
     binaryString = ''.join(str(x) for x in data)
     binaryList = [binaryString[i:i+8] for i in range(0, len(binaryString), 8)]
     intList = []
@@ -79,19 +89,16 @@ def binaryToText(data):
     return result
 
 
-def asciiTest(data):
-    binaryString = ''.join(str(x) for x in data)
-    binaryList = [binaryString[i:i+8] for i in range(0, len(binaryString), 8)]
-    for each in binaryList:
-        print(str(chr(int(each, 2))))
-
 def checkStorageSize():
+    """Check maximum amount of data that can be encoded into a given image."""
     width = key.getWidth()
     height = key.getHeight()
     maxSize = width * height
     return maxSize
 
+
 def flipLSB(pixel):
+    """Invert the LSB of the red value of a pixel."""
     tmp = pixel.getRed()
     if (tmp > 0):
         tmp -= 1
@@ -108,12 +115,16 @@ if (len(textToBinary(StringToEncode)) <= checkStorageSize()):
     if(str(encodeCheck) == str(StringToEncode)):
         print("Data successfully encoded")
     else:
-        backup.save("Encoded.png")
+        backup.save(EncodedFileName + ".png")
         print("Encoded.png reverted to backup")
-        raise ValueError("Encoding/Decoding failed " + "\'" + encodeCheck + "\'" + " != " + "\'" + StringToEncode + "\'")
+        raise ValueError("Encoding/Decoding failed " + "\'" + encodeCheck
+                         + "\'" + " != " + "\'" + StringToEncode + "\'")
 else:
     backup.save('Encoded.png')
     print('Encoded.png reverted to backup')
-    raise ValueError('Input data is too large for given image. Your data is ' + str(len(textToBinary(StringToEncode))) + ' bits. Make sure that data is less than ' + str(checkStorageSize()) + " bits")
+    raise ValueError('Input data is too large for given image. Your data is '
+                     + str(len(textToBinary(StringToEncode)))
+                     + ' bits. Make sure that data is less than '
+                     + str(checkStorageSize()) + " bits")
 endTime = time()
 print("Data encoded and checked in: " + str(endTime - startTime) + " seconds")
