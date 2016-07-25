88aa680d8e96ab0c8f8f3671481be2e1b22ee6a8
Matthew Tilton <mtilton1996@gmail.com>
2016-07-24 23:41:11 -0500

    added various error checking

--- pySteg.py
+++ pySteg.py
@@ -5,29 +5,29 @@
 # TODO Add variable for path to master.
 # TODO Add variable for path to result.
 # TODO Add interface.
-# TODO Add offset
+# TODO Add offset. In Progress
 # TODO Add random seed
 # TODO Add timer. DONE
-# TODO Add error check
+# TODO Add error check. DONE
 # TODO Organize
-# TODO Add Docstring
+# TODO Add Docstrings
+# TODO Add check for max data size. DONE
 
 from cImage import FileImage
 from cImage import EmptyImage
 from cImage import Pixel
 from time import time
 
-encoded = FileImage("Encoded.png")
 key = FileImage("master.png")
-result = EmptyImage(key.getWidth(), key.getHeight())
-
+backup = FileImage("Encoded.png")
 
 def Encode(data):
+    result = EmptyImage(key.getWidth(), key.getHeight())
     count = 0
     for row in range(key.getHeight()):
         for col in range(key.getWidth()):
             keyPixel = key.getPixel(col, row)
-            if count < (len(data) - 1):
+            if count < (len(data)):
                 if (int(data[count]) == 1):
                     newPixel = flipLSB(keyPixel)
                 else:
@@ -40,6 +40,7 @@ def Encode(data):
 
 
 def Decode():
+    encoded = FileImage("Encoded.png")
     result = []
     for row in range(encoded.getHeight()):
         for col in range(encoded.getWidth()):
@@ -58,7 +59,6 @@ def Decode():
 
 def textToBinary(data):
     result = ''.join(format(ord(x), '08b') for x in data)
-    print(result)
     return str(result)
 
 
@@ -73,7 +73,7 @@ def binaryToText(data):
         charList.append(str(chr(each)))
     cleanCharList = []
     for each in charList:
-        if each != '\x00':
+        if each is not '\x00':
             cleanCharList.append(each)
     result = ''.join(str(x) for x in cleanCharList)
     return result
@@ -85,6 +85,11 @@ def asciiTest(data):
     for each in binaryList:
         print(str(chr(int(each, 2))))
 
+def checkStorageSize():
+    width = key.getWidth()
+    height = key.getHeight()
+    maxSize = width * height
+    return maxSize
 
 def flipLSB(pixel):
     tmp = pixel.getRed()
@@ -96,12 +101,19 @@ def flipLSB(pixel):
     return pixel
 
 startTime = time()
-StringToEncode = "go fuck yourselfgghasd"
-Encode(textToBinary(StringToEncode))
-encodeCheck = binaryToText(Decode())
-if(encodeCheck == StringToEncode):
-    print(encodeCheck)
+StringToEncode = "test string"
+if (len(textToBinary(StringToEncode)) <= checkStorageSize()):
+    Encode(textToBinary(StringToEncode))
+    encodeCheck = binaryToText(Decode())
+    if(str(encodeCheck) == str(StringToEncode)):
+        print("Data successfully encoded")
+    else:
+        backup.save("Encoded.png")
+        print("Encoded.png reverted to backup")
+        raise ValueError("Encoding/Decoding failed " + "\'" + encodeCheck + "\'" + " != " + "\'" + StringToEncode + "\'")
 else:
-    raise ValueError("Encoding/Decoding failed " + StringToEncode + " != " + encodeCheck)
+    backup.save('Encoded.png')
+    print('Encoded.png reverted to backup')
+    raise ValueError('Input data is too large for given image. Your data is ' + str(len(textToBinary(StringToEncode))) + ' bits. Make sure that data is less than ' + str(checkStorageSize()) + " bits")
 endTime = time()
 print("Data encoded and checked in: " + str(endTime - startTime) + " seconds")
