18bed32c9bde858ba4b6d23bbf8982ff587b2587
Matthew Tilton <mtilton1996@gmail.com>
2016-07-24 22:44:33 -0500

    Some more work. Added TODO

--- Encoder.py
+++ Encoder.py
@@ -1,15 +1,82 @@
+# TODO Consolidate file. DONE
+# TODO Make classes.
+# TODO Add variable for file name.
+# TODO Add variable for file type.
+# TODO Add variable for path to master.
+# TODO Add variable for path to result.
+# TODO Add interface.
+# TODO Add offset
+# TODO Add random seed
+# TODO Add timer. DONE
+# TODO Add error check
+# TODO Organize
+# TODO Add Docstring
+
 from cImage import FileImage
 from cImage import EmptyImage
 from cImage import Pixel
+from time import time
 
-master = FileImage("master.png")
-result = EmptyImage(master.getWidth(), master.getHeight())
+encoded = FileImage("Encoded.png")
+key = FileImage("master.png")
+result = EmptyImage(key.getWidth(), key.getHeight())
+
+
+def Encode(data):
+    count = 0
+    for row in range(key.getHeight()):
+        for col in range(key.getWidth()):
+            keyPixel = key.getPixel(col, row)
+            if count < (len(data) - 1):
+                if (int(data[count]) == 1):
+                    newPixel = flipLSB(keyPixel)
+                else:
+                    newPixel = keyPixel
+                count += 1
+                result.setPixel(col, row, newPixel)
+            else:
+                result.setPixel(col, row, keyPixel)
+    result.save("Encoded.png")
+
+
+def Decode():
+    result = []
+    for row in range(encoded.getHeight()):
+        for col in range(encoded.getWidth()):
+            encodedPixel = encoded.getPixel(col, row)
+            keyPixel = key.getPixel(col, row)
+
+            # 1
+            if encodedPixel.getRed() != keyPixel.getRed():
+                result.append(1)
+
+            # 0
+            else:
+                result.append(0)
+    return result
 
 
 def textToBinary(data):
     result = ''.join(format(ord(x), '08b') for x in data)
     print(result)
     return str(result)
+
+
+def binaryToText(data):
+    binaryString = ''.join(str(x) for x in data)
+    binaryList = [binaryString[i:i+8] for i in range(0, len(binaryString), 8)]
+    intList = []
+    for each in binaryList:
+        intList.append(int(each, 2))
+    charList = []
+    for each in intList:
+        charList.append(str(chr(each)))
+    cleanCharList = []
+    for each in charList:
+        if each != '\x00':
+            cleanCharList.append(each)
+    result = ''.join(str(x) for x in cleanCharList)
+    return result
 
 
 def asciiTest(data):
@@ -19,23 +86,6 @@ def asciiTest(data):
         print(str(chr(int(each, 2))))
 
 
-def Encode(data):
-    count = 0
-    for row in range(master.getHeight()):
-        for col in range(master.getWidth()):
-            masterPixel = master.getPixel(col, row)
-            if count < (len(data) - 1):
-                if (int(data[count]) == 1):
-                    newPixel = flipLSB(masterPixel)
-                else:
-                    newPixel = masterPixel
-                count += 1
-                result.setPixel(col, row, newPixel)
-            else:
-                result.setPixel(col, row, masterPixel)
-    result.save("Encoded.png")
-
-
 def flipLSB(pixel):
     tmp = pixel.getRed()
     if (tmp > 0):
@@ -43,7 +93,15 @@ def flipLSB(pixel):
     else:
         tmp += 1
     pixel.setRed(tmp)
-    # pixel.setRed(255)
     return pixel
 
-Encode(textToBinary("go fuck yourself"))
+startTime = time()
+StringToEncode = "go fuck yourselfgghasd"
+Encode(textToBinary(StringToEncode))
+encodeCheck = binaryToText(Decode())
+if(encodeCheck == StringToEncode):
+    print(encodeCheck)
+else:
+    raise ValueError("Encoding/Decoding failed " + StringToEncode + " != " + encodeCheck)
+endTime = time()
+print("Data encoded and checked in: " + str(endTime - startTime) + " seconds")
